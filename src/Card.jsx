a94e993e27a494937dfaf1d30f5cf9a2b8f028f4
abdulrahmanraziq <abdulrahmanraziq548@gmail.com>
2024-05-12 12:56:13 +0530

    My React App

---
+++ src/Card.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Card({imageUrl, title, description}) {
+  return <>
+      <div className="col-lg-4 col-sm-6 mb-4">
+        <div className="card h-100">
+          <a href="#">
+            <img
+              className="card-img-top"
+              src={imageUrl}
+              alt={title}
+              width={"200px"}
+              height={"200px"}
+            />
+          </a>
+          <div className="card-body">
+            <h4 className="card-title">
+              <a href="#">{title}</a>
+            </h4>
+            <p className="card-text">{description}</p>
+          </div>
+        </div>
+      </div>
+      </>
+}
+
+export default Card;
